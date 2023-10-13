aeea360294221706125f3d69d479f18e4466133d
Michael Johne <mjohne@users.noreply.github.com>
2023-10-13 05:11:37 +0200

    Add files via upload

--- Black'n'White/Program.cs
+++ Black'n'White/Program.cs
@@ -3,29 +3,28 @@ using System.Windows.Forms;
 
 namespace BlackAndWhite
 {
-	/// <summary>
-	/// Program
-	/// </summary>
-	internal static class Program
-	{
-		/// <summary>
-		/// main entrance point of the application
-		/// </summary>
-		[STAThread]
-		private static void Main()
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(defaultValue: false);
-			using (BlackAndWhiteForm mainForm = new BlackAndWhiteForm())
-			{
-				Application.Run(mainForm: mainForm);
-			}
-		}
-	}
+    /// <summary>
+    /// Program
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// main entrance point of the application
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(defaultValue: false);
+            using (BlackAndWhiteForm mainForm = new BlackAndWhiteForm())
+            {
+                Application.Run(mainForm: mainForm);
+            }
+        }
+    }
 }
 
-//todo: highscore
-//todo: iconset, language, settings
-//todo: rightclick on field for inverting
-//todo: gameboard 6x6
+//todo: high score
+//todo: icon set, language, settings
+//todo: game board 6x6
 //todo: sound?
